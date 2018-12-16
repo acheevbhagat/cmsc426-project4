7b2d817412f4ec13496f4d56da2f7ebf38e64a25
Chris <chris.yue1998@gmail.com>
2018-12-16 17:35:29 -0500

    Add starter code.

---
+++ code/SLAMusingGTSAM.m
@@ -0,0 +1,12 @@
+function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSize, qIMUToC, TIMUToC,...
+                                                IMU, LeftImgs, TLeftImgs, Mode)
+% For Input and Output specifications refer to the project pdf
+
+import gtsam.*
+% Refer to Factor Graphs and GTSAM Introduction
+% https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam.pdf
+% and the examples in the library in the GTSAM toolkit. See folder
+% gtsam_toolbox/gtsam_examples
+
+
+end
---
+++ code/Wrapper.m
@@ -0,0 +1,20 @@
+%% Wrapper for the CMSC426Course final project at University of Maryland, College Park
+% Code by: Nitin J. Sanket (nitinsan@terpmail.umd.edu)
+%          (Originally for CMSC828T)
+
+clc
+clear all
+close all
+
+%% Add ToolBox to Path eg. ToolboxPath = 'gtsam_toolbox';
+addpath(ToolboxPath);
+
+%% Load Data
+% Download data from the following link: 
+% https://drive.google.com/open?id=1ZFXZEv4yWgaVDE1JD6-oYL2KQDypnEUU
+load('DataMapping.mat');
+load('CalibParams.mat');
+
+%% SLAM Using GTSAM
+[LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSize, qIMUToC, TIMUToC,...
+                                                IMU, LeftImgs, TLeftImgs);
