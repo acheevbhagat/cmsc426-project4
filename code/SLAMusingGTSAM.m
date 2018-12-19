9539131a17979bd87489a125ce16b22e959af6e5
Chris <chris.yue1998@gmail.com>
2018-12-19 16:27:29 -0500

    Add visualizations.

--- code/SLAMusingGTSAM.m
+++ code/SLAMusingGTSAM.m
@@ -22,10 +22,13 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
         frames{f} = frame;
     end
     
+    
+    homographies = cell(numfiles, 1);
+    
     % Load mapping data
     frame_one_detections = DetAll{1};
     
-    % Detection data stores as [TagID, p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y]
+    % Detection data stored as [TagID, p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y]
     first_col = frame_one_detections(:, 1);
     tag_10_data = frame_one_detections(first_col == 10, :);
     p1x = tag_10_data(2);
@@ -42,25 +45,28 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
     %origin_coords = [[0 0 1]' [TagSize 0 1]' [TagSize TagSize 1]' [0 TagSize 1]'];
     
     % Initialize camera pose by calculating homography
-    tform = estimateGeometricTransform(tag_10_coords, origin_coords, 'projective')
-    KH = tform.T;
-    %KH = homography2d(tag_10_coords, inv(K) * origin_coords);
-    %KH * tag_10_coords(:, 1)
-    H = K \ KH % Equivalent to H = inv(K) * KH
-    h_1 = H(:, 1);
-    h_2 = H(:, 2);
-    h_3 = H(:, 3);
-    % SVD on H to find the Rotation (R) and Translation (T) values for pose
-    [U, S, V] = svd([h_1 h_2 cross(h_1, h_2)]);
-    R = U * [1 0 0; 0 1 0; 0 0 det(U * V')] * V';
-    T = h_3 / norm(h_1);
-    pose = [R T]
-    quat = v_rotro2qr(R);
-    AllPosesComputed = [AllPosesComputed; T(1) T(2) T(3) quat(1) quat(2) quat(3) quat(4)];
+    tform = estimateGeometricTransform(tag_10_coords, origin_coords, 'projective');
+    homographies{1} = tform.T;
+    
+%     KH = tform.T;
+%     %KH = homography2d(tag_10_coords, inv(K) * origin_coords);
+%     %KH * tag_10_coords(:, 1)
+%     H = K \ KH % Equivalent to H = inv(K) * KH
+%     h_1 = H(:, 1);
+%     h_2 = H(:, 2);
+%     h_3 = H(:, 3);
+%     % SVD on H to find the Rotation (R) and Translation (T) values for pose
+%     [U, S, V] = svd([h_1 h_2 cross(h_1, h_2)]);
+%     R = U * [1 0 0; 0 1 0; 0 0 det(U * V')] * V';
+%     T = h_3 / norm(h_1);
+%     pose = [R T]
+    
     %world_frame_coords = [10, 0, 0, TagSize, 0, TagSize, TagSize, 0, TagSize];
     %imshow(frames{1});
     hold on;
     prev_world_coords = [];
+    img_pts = [];
+    world_pts = [];
     for f = 1:size(frame_one_detections, 1)
         tag_data = frame_one_detections(f, :);
         tag_id = tag_data(1);
@@ -74,21 +80,29 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
         p4y = tag_data(9);
         tag_xs = [p1x p2x p3x p4x];
         tag_ys = [p1y p2y p3y p4y];
+        img_pts = [img_pts; p1x p1y; p2x p2y; p3x p3y; p4x p4y];
         % Code to check homography accuracy
         [xs, ys] = transformPointsForward(tform, tag_xs', tag_ys');
+        world_pts = [world_pts; xs(1) ys(1) 0; xs(2) ys(2) 0; xs(3) ys(3) 0; xs(4) ys(4) 0];
         x = [xs(1) xs(2) xs(3) xs(4) xs(1)];
         y = [ys(1) ys(2) ys(3) ys(4) ys(1)];
         prev_world_coords = [prev_world_coords; tag_id xs(1) ys(1) xs(2) ys(2) xs(3) ys(3) xs(4) ys(4)];
         %plot(x, y, 'b-', 'LineWidth', 2);
     end
+    
+    params = cameraParameters('IntrinsicMatrix', K');
+    [R, T] = estimateWorldCameraPose(img_pts, ...
+    world_pts, params, 'Confidence', 70);
+    pose = [R T'];
+    
+    quat = v_rotro2qr(R);
+    AllPosesComputed = [AllPosesComputed; T(1) T(2) T(3) quat(1) quat(2) quat(3) quat(4)];
     hold off;
     LandMarksComputed = [LandMarksComputed; prev_world_coords];
     
     % Run through the images and update on each image
-    homographies = cell(numfiles - 1, 1);
-    
     for f = 2:numfiles
-        disp(f)
+        %disp(f)
         % Find common landmarks between the frames
         currLandmarks = DetAll{f};
         prevLandmarks = prev_world_coords;
@@ -141,26 +155,11 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
         
         %H = homography2d(currInfo, prevInfo);
         tform = estimateGeometricTransform(currInfo', prevInfo', 'projective');
-        KH = tform.T;
-        H = K \ KH; % Equivalent to H = inv(K) * KH
-        h_1 = H(:, 1);
-        h_2 = H(:, 2);
-        h_3 = H(:, 3);
-        % SVD on H to find the Rotation (R) and Translation (T) values for pose
-        [U, S, V] = svd([h_1 h_2 cross(h_1, h_2)]);
-        R = U * [1 0 0; 0 1 0; 0 0 det(U * V')] * V';
-        T = h_3 / norm(h_1);
-        pose = [R T];
-        quat = v_rotro2qr(R);
-        AllPosesComputed = [AllPosesComputed; T(1) T(2) T(3) quat(1) quat(2) quat(3) quat(4)];
-        %params = cameraParameters('IntrinsicMatrix', K');
-        %currWorldCoords = inv(H) * tagCoords';
-        %[orientation, location] = estimateWorldCameraPose(imagePoints, ...
-        %currWorldCoords', params);
-        %pose = [orientation location'];
+        homographies{f} = tform.T;
+        
         prev_world_coords = [];
-        tag_xs = [];
-        tag_ys = [];
+        img_pts = [];
+        world_pts = [];
         for i = 1:size(currLandmarks, 1)
             tag_data = currLandmarks(i, :);
             tag_id = tag_data(1);
@@ -172,14 +171,24 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
             p3y = tag_data(7);
             p4x = tag_data(8);
             p4y = tag_data(9);
+            img_pts = [img_pts; p1x p1y; p2x p2y; p3x p3y; p4x p4y];
             tag_xs = [p1x p2x p3x p4x];
             tag_ys = [p1y p2y p3y p4y];
             [xs, ys] = transformPointsForward(tform, tag_xs', tag_ys');
+            world_pts = [world_pts; xs(1) ys(1) 0; xs(2) ys(2) 0; xs(3) ys(3) 0; xs(4) ys(4) 0];
             x = [xs(1) xs(2) xs(3) xs(4) xs(1)];
             y = [ys(1) ys(2) ys(3) ys(4) ys(1)];
             prev_world_coords = [prev_world_coords; tag_id xs(1) ys(1) xs(2) ys(2) xs(3) ys(3) xs(4) ys(4)];
             %plot(x, y, 'b-', 'LineWidth', 2);
         end
+        
+        params = cameraParameters('IntrinsicMatrix', K');
+        [R, T] = estimateWorldCameraPose(img_pts, ...
+            world_pts, params, 'Confidence', 70);
+        pose = [R T'];
+
+        quat = v_rotro2qr(R);
+        AllPosesComputed = [AllPosesComputed; T(1) T(2) T(3) quat(1) quat(2) quat(3) quat(4)];
         
         for p = 1:size(prev_world_coords, 1)
             if ~ismember(prev_world_coords(p, 1), LandMarksComputed(:, 1))
@@ -220,7 +229,7 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
         p3x = tag_data(6);
         p4x = tag_data(8);
         tag_xs = [p1x p2x p3x p4x p1x];
-        tag_zs = [1 1 1 1 1];
+        tag_zs = [0 0 0 0 0];
         plot(tag_xs, tag_zs, 'b-', 'LineWidth', 2);
     end
     
@@ -234,6 +243,7 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
     end
     hold off;
     
+    
     % Factor Graph Section
     x = cell(length(DetAll), 1);
     for f = 1:length(DetAll)
@@ -241,13 +251,13 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
     end
     
     % Variables to store landmark points
-    all_frames_landmarks = cell(length(DetAll), 1);
+    all_frames_landmarks = cell(length(LandMarksComputed), 1);
     for f = length(DetAll)
-        cur_frame_landmarks = sortrows(frame_one_detections, 1);
+        cur_frame_landmarks = sortrows(DetAll{f}, 1);
         p = cell(length(cur_frame_landmarks),1);
-        point_count = 0
+        point_count = 0;
         for f = 1:length(cur_frame_landmarks(:, 1))
-            for j = 1:length(cur_frame_landmarks(f, 2:length(A)))
+            for j = 1:length(cur_frame_landmarks(f, 2:length()))
                 point_count = point_count + 1;
                 p{point_count} = symbol('lp', cur_frame_landmarks(point_count, 1));
             end
@@ -259,7 +269,7 @@ function [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSi
 
     % Add prior
     priorMean = Pose2(0.0, 0.0, 0.0); % prior at origin
-    priorNoise = noiseModel.Diagonal.Sigmas([0.3; 0.3; 0.1])
+    priorNoise = noiseModel.Diagonal.Sigmas([0.3; 0.3; 0.1]);
     graph.add(PriorFactorPose2(x{1}, priorMean, priorNoise));
     
     % Add odometry between steps
